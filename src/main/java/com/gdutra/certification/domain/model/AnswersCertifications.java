0dc6db79ae0e0134edd7e589b45ff6533c4fb463
Dutra3 <gabrieldutra45@gmail.com>
2024-02-10 23:04:04 -0300

    Add data base config to create entites table

--- src/main/java/com/gdutra/certification/domain/model/AnswersCertifications.java
+++ src/main/java/com/gdutra/certification/domain/model/AnswersCertifications.java
@@ -1,20 +1,47 @@
 package com.gdutra.certification.domain.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswersCertifications {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "certification_id")
     private UUID certificationId;
+
+    @Column(name = "student_id")
     private UUID studentId;
+
+    @Column(name = "question_id")
     private UUID questionId;
+
+    @Column(name = "answer_id")
     private UUID answerId;
+
+    @Column(name = "is_correct")
     private Boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    private CertificationStudent certificationStudent;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private Student student;
 }
--- src/main/java/com/gdutra/certification/domain/model/Student.java
+++ src/main/java/com/gdutra/certification/domain/model/Student.java
@@ -1,18 +1,31 @@
 package com.gdutra.certification.domain.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "student")
     private List<CertificationStudent> certificationStudents;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
